13c40fcc8b84ff4d39a2deecd4b2fe9eb1b7e08a
graceFor <tbgusrud12@gmail.com>
2020-08-12 17:15:31 +0900

    [index.html update]

--- Web/Plant_project/Plant/urls.py
+++ Web/Plant_project/Plant/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import plantapp.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
